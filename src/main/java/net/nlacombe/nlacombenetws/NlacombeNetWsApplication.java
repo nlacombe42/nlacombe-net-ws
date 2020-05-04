09bbdd4c7ebae84c402cee842c710e050fe815d6
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2020-05-03 21:39:06 -0400

    added boilerplate

---
+++ src/main/java/net/nlacombe/nlacombenetws/NlacombeNetWsApplication.java
@@ -0,0 +1,12 @@
+package net.nlacombe.nlacombenetws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlacombeNetWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlacombeNetWsApplication.class, args);
+    }
+}
