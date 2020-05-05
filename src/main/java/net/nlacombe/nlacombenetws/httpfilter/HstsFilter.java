d35daf14628a08562ede890f7f9dd0c57f376de7
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2020-05-05 00:26:53 -0400

    added HSTS HTTP filter

---
+++ src/main/java/net/nlacombe/nlacombenetws/httpfilter/HstsFilter.java
@@ -0,0 +1,24 @@
+package net.nlacombe.nlacombenetws.httpfilter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class HstsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var httpServletResponse = (HttpServletResponse) servletResponse;
+
+        httpServletResponse.setHeader("Strict-Transport-Security", "max-age=15768000; includeSubDomains");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
